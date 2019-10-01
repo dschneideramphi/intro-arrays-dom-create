8af98e42b1cb0e46c992342ff54d273b654718bc
dschneideramphi <dschneider@amphi.com>
2019-10-01 11:39:31 -0700

    Update string-split.js

--- string-split/string-split.js
+++ string-split/string-split.js
@@ -1,13 +1,3 @@
 //string-split.js
 
 let containerElt = document.querySelector("#container");
-
-let userInput = prompt("Enter a sentence");
-
-let words = userInput.split(" ");
-
-for(w of words) {
-  let newP = document.createElement("p");
-  newP.innerHTML = w;
-  containerElt.appendChild(newP);
-}
