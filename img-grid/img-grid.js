319710c26ba1c175b89a956a34d9c5506719613b
dschneideramphi <dschneider@amphi.com>
2019-10-01 11:37:04 -0700

    img-grid files

---
+++ img-grid/img-grid.js
@@ -0,0 +1,30 @@
+//img-grid.js
+
+/*
+Your Task: Use an array of image URLs to dynamically create an image grid
+
+Code you will need:
+[]
+for(__ of ___) {
+
+}
+document.createElement()
+document.querySelector()
+____.appendChild()
+____.src
+
+*/
+
+
+//Step 1: Create a variable for the container element on the screen
+
+//Step 2: Create an array of image URLs. For now, just start with 3 images but then you can add more later
+
+//Step 3: Iterate through your array of images:
+  //Step 4: Create an image element for each URL
+  //Step 5: Use the URL as the image source
+  //Step 6: Append the image to the container element
+
+//Step 7: Test
+//Step 8: Add lots more images, then test again
+//Step 9: Celebrate
